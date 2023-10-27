13320bac9f516b643da8fb6db83fc7d50e8dd8c2
Eduardo Lima <natos.10@hotmail.com>
2023-10-27 12:27:47 -0300

    config Search field

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path("created_by/<int:author_pk>/", views.created_by, name="created_by"),
     path("category/<slug:slug>/", views.category, name="category"),
     path("tag/<slug:slug>/", views.tag, name="tag"),
+    path("search/", views.search, name="search"),
 ]
