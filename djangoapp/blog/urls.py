ecbb15d447ada6a1a1e34a28df51c7805d973b25
Eduardo Lima <natos.10@hotmail.com>
2023-10-27 12:14:59 -0300

    using ManyToManyField within the Django template for tags

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path("page/<slug:slug>/", views.page, name="page"),
     path("created_by/<int:author_pk>/", views.created_by, name="created_by"),
     path("category/<slug:slug>/", views.category, name="category"),
+    path("tag/<slug:slug>/", views.tag, name="tag"),
 ]
