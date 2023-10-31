179487421aba7400bec9933beed07686bc74f63f
Eduardo Lima <natos.10@hotmail.com>
2023-10-31 10:43:06 -0300

    update titles

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,6 +1,8 @@
 from blog.models import Page, Post
+from django.contrib.auth.models import User
 from django.core.paginator import Paginator
 from django.db.models.query_utils import Q
+from django.http import Http404
 from django.shortcuts import render
 
 PER_PAGE = 9
@@ -18,12 +20,20 @@ def index(request):
         "blog/pages/index.html",
         {
             "page_obj": page_obj,
+            "page_title": "Home - ",
         },
     )
 
 
 def created_by(request, author_pk):
+    user = User.objects.filter(pk=author_pk).first()
+    if user is None:
+        raise Http404()
     posts = Post.objects.get_published().filter(created_by__pk=author_pk)
+    user_full_name = user.username
+    if user.first_name:
+        user_full_name = f"{user.first_name} {user.last_name}"
+    page_title = f"Posts de {user_full_name}  - "
 
     paginator = Paginator(posts, PER_PAGE)
     page_number = request.GET.get("page")
@@ -34,6 +44,7 @@ def created_by(request, author_pk):
         "blog/pages/index.html",
         {
             "page_obj": page_obj,
+            "page_title": page_title,
         },
     )
 
@@ -45,11 +56,17 @@ def category(request, slug):
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
 
+    if len(page_obj) == 0:
+        raise Http404()
+
+    page_title = f"{page_obj[0].category.name} - Categoria -"
+
     return render(
         request,
         "blog/pages/index.html",
         {
             "page_obj": page_obj,
+            "page_title": page_title,
         },
     )
 
@@ -61,11 +78,17 @@ def tag(request, slug):
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
 
+    if len(page_obj) == 0:
+        raise Http404()
+
+    page_title = f"{page_obj[0].tags.first().name} - Tag -"
+
     return render(
         request,
         "blog/pages/index.html",
         {
             "page_obj": page_obj,
+            "page_title": page_title,
         },
     )
 
@@ -78,34 +101,50 @@ def search(request):
         | Q(excerpt__icontains=search_value)
     )[:PER_PAGE]
 
+    page_title = f"{search_value[:15]} - Search -"
+
     return render(
         request,
         "blog/pages/index.html",
         {
             "page_obj": posts,
             "search_value": search_value,
+            "page_title": page_title,
         },
     )
 
 
 def page(request, slug):
-    page = Page.objects.filter(is_published=True).filter(slug=slug).first()
+    page_obj = Page.objects.filter(is_published=True).filter(slug=slug).first()
+
+    if page_obj is None:
+        raise Http404()
+
+    page_title = f"{page_obj.title} - Página -"
+
     return render(
         request,
         "blog/pages/page.html",
         {
-            "page": page,
+            "page": page_obj,
+            "page_title": page_title,
         },
     )
 
 
 def post(request, slug):
-    post = Post.objects.get_published().filter(slug=slug).first()
+    post_obj = Post.objects.get_published().filter(slug=slug).first()
+
+    if post_obj is None:
+        raise Http404()
+
+    page_title = f"{post_obj.title} - Post -"
 
     return render(
         request,
         "blog/pages/post.html",
         {
-            "post": post,
+            "post": post_obj,
+            "page_title": page_title,
         },
     )
